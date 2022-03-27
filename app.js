b5986e52a70028dad56691f9afdb8252826e0449
Abdel Raoof <olakara@gmail.com>
2022-03-27 22:12:13 +0400

    create people and dump to json

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+import * as fs from 'fs';
 import { faker } from '@faker-js/faker';
 import { getPerson, getFamily } from './people.js';
 import { getEmployee } from './employee.js';
 
 
 //console.log(getPerson());
-console.log(getEmployee());
+//console.log(getEmployee());
 //console.log(getFamily());
 
+
+
+// create people
+console.log('Creating people...');
+let result = [];
+for (let i = 0; i < 500000; i++) {
+    result.push(getPerson());
+}
+try {
+    console.log('Writing file..');
+    fs.writeFileSync('./people.json', JSON.stringify(result));
+  } catch (err) {
+    console.error(err);
+  }
+console.log('done!')
 
 
