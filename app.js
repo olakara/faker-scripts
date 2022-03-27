6ddce734b4377527d4ffb5cde3c91a7a8a8104d6
Abdel Raoof <olakara@gmail.com>
2022-03-27 23:23:14 +0400

    updates

--- app.js
+++ app.js
@@ -2,26 +2,40 @@ import * as fs from 'fs';
 import { faker } from '@faker-js/faker';
 import { getPerson, getFamily } from './people.js';
 import { getEmployee } from './employee.js';
+import { getProduct } from './products.js';
 
-
+//console.log(getProduct());
 //console.log(getPerson());
-//console.log(getEmployee());
-//console.log(getFamily());
+console.log(getEmployee());
+
+
 
 
 
 // create people
-console.log('Creating people...');
+// console.log('Creating people...');
+// let result = [];
+// for (let i = 0; i < 500000; i++) {
+//     result.push(getPerson());
+// }
+// try {
+//     console.log('Writing file..');
+//     fs.writeFileSync('./people.json', JSON.stringify(result));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// console.log('done!')
+
+//create employee
+console.log('Creating Employee...');
 let result = [];
-for (let i = 0; i < 500000; i++) {
-    result.push(getPerson());
+for (let i = 0; i < 500; i++) {
+    result.push(getEmployee());
 }
 try {
     console.log('Writing file..');
-    fs.writeFileSync('./people.json', JSON.stringify(result));
+    fs.writeFileSync('./employees.json', JSON.stringify(result));
   } catch (err) {
     console.error(err);
   }
 console.log('done!')
-
-
--- employee.js
+++ employee.js
@@ -11,12 +11,43 @@ function getMobile() {
   return faker.phone.phoneNumber('+971-5#-#######');
 }
 
+function getDepartment() {  
+  return faker.random.arrayElement(['Adminstration','Finance','Human Resource','Inventory','Logistics','Manufacturing','Procurement'])
+}
+
+function getDepartmentSection() {
+  return faker.name.jobArea();
+}
+
+function getManufacturingSection() {
+  
+  let assembly = faker.random.arrayElement(['AS1','AS2','AS4','AS5','AS6']);
+  let unit = faker.random.arrayElement(['U1','U2','U3','U4','U5','U6','U7','U8']);
+
+  return assembly + ' - '+ unit;
+}
+function getJoiningDate(dob) {
+
+  let birthYear = dob.getFullYear();
+  let workerYear = birthYear + 25;
+  let currentYear = new Date().getFullYear();
+  let startYear = faker.datatype.number({ min: workerYear, max:  currentYear});
+  if(startYear >= currentYear) {
+    startYear = currentYear - faker.datatype.number({ min: 1, max:  3});
+  }
+  let somePastDate = faker.date.past();
+  return new Date(startYear, somePastDate.getMonth(), somePastDate.getDate());
+}
+
 function getEmployee() {
 
     let person = getPerson(undefined, 'worker');    
     person.eid = getEmployeeId();
     person.mobile = getMobile();
-  
+    person.department = getDepartment();
+    person.joinDate = getJoiningDate(person.dob);
+    person.jobType = faker.name.jobType();
+    person.section = person.department != 'Manufacturing' ? getDepartmentSection() : getManufacturingSection();
     return person;
   }
 
